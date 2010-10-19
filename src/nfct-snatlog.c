cce343ed199a7893f3187206b9c94d944dd7477b
Italo Valcy <italo@dcc.ufba.br>
2010-10-18 22:32:45 -0300

    a lot of changes

--- src/nfct-snatlog.c
+++ src/nfct-snatlog.c
@@ -28,34 +28,191 @@
 #include <time.h>
 #include <arpa/inet.h>
 #include <libnetfilter_conntrack/libnetfilter_conntrack.h>
+#include <getopt.h>
+#include <syslog.h>
 
 #include "list.h"
 
 #define PROGNAME "nfct-snatlog"
 
-// length("%Y-%m-%d %H:%M:%S %z") = 25
-#define LENGTH_ASCTIME 25
+#define BUF_LEN 1024
 
 #define proto_str(u8_proto) (u8_proto==IPPROTO_TCP) ? "tcp" : "udp"
 
+#define BUFFER_SIZE(ret, size, len, offset)		\
+	size += ret;					\
+	if (ret > len)					\
+		ret = len;				\
+	offset += ret;					\
+	len -= ret;
+
 static struct nfct_handle *cth;
 
-static int debug = 1;
+static u_int8_t debug_flag = 0;
+static u_int8_t daemon_flag = 0;
+static int syslog_facility = LOG_LOCAL4;
 
 struct conntrack_list *ct_list = NULL;
+
+
+void usage() {
+   printf("Usage: %s [options]\n\n");
+   printf("Options:\n");
+   printf("  -s, --daemon\t\t\tRun %s as a daemon.\n");
+   printf("  -d, --debug\t\t\tPrint debug messages.\n");
+   printf("  -f, --facility FACILITY\t\t\tSyslog facility (default: LOCAL4)\n");
+   printf("  -h, --help\t\t\tDisplay a short help messsage\n");
+   printf("\nFor a more detailed information, see %s(8)\n", PROGNAME);
+}
 
 char * net2addr(u_int32_t u32_addr) {
    struct in_addr addr = { .s_addr = u32_addr};
    return inet_ntoa(addr);
 }
 
+int __snprintf_start_log(char *buf, unsigned int len, char *log_type) {
+   time_t now;
+   int ret, size = 0, offset = 0;
+
+   // if we run in daemon mode, we does not need print
+   // the timestamp part... syslog does it for us. Otherwise
+   // this code is needed
+   if (!daemon_flag) {
+      time(&now);
+      ret = strftime(buf, len, "%Y-%m-%d %H:%M:%S %z", localtime(&now));
+      BUFFER_SIZE(ret, size, len, offset);
+
+      ret = snprintf(buf+offset, len, " %s: ", PROGNAME, log_type);
+      BUFFER_SIZE(ret, size, len, offset);
+   }
+
+   ret = snprintf(buf+offset, len, " [%s]", log_type);
+   BUFFER_SIZE(ret, size, len, offset);
+
+   return size;
+}
+
+int __str2facility(char *str) {
+   if (strcmp(str,"LOCAL0")) return LOG_LOCAL0;
+   if (strcmp(str,"LOCAL1")) return LOG_LOCAL1;
+   if (strcmp(str,"LOCAL2")) return LOG_LOCAL2;
+   if (strcmp(str,"LOCAL3")) return LOG_LOCAL3;
+   if (strcmp(str,"LOCAL4")) return LOG_LOCAL4;
+   if (strcmp(str,"LOCAL5")) return LOG_LOCAL5;
+   if (strcmp(str,"LOCAL6")) return LOG_LOCAL6;
+   if (strcmp(str,"LOCAL7")) return LOG_LOCAL7;
+   if (strcmp(str,"USER"))   return LOG_USER;
+   if (strcmp(str,"DAEMON")) return LOG_DAEMON;
+   return -1;
+}
+
+void print_snatlog(struct nf_conntrack *ct, 
+      time_t *timestamp, char *proto_str) {
+   int ret = 0, size = 0, offset = 0, len = BUF_LEN;
+   char buf[BUF_LEN];
+
+   ret = __snprintf_start_log(buf, len, "SNAT_LOG");
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " proto=%s", proto_str);
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-src=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-sport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " trans-src=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " trans-sport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " duration=%.0lfs", 
+         difftime(time(NULL),*timestamp));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   buf[size+1 > len ? len-1 : size] = '\0';
+
+   printf("%s\n",buf);
+   fflush(stdout);
+}
+
+void print_debug(struct nf_conntrack *ct, 
+      enum nf_conntrack_msg_type type, char *proto_str) {
+   int ret = 0, size = 0, offset = 0, len = BUF_LEN;
+   char buf[BUF_LEN];
+
+   ret = __snprintf_start_log(buf, len, "DEBUG");
+   BUFFER_SIZE(ret, size, len, offset);
+
+   switch(type) {
+      case NFCT_T_NEW:
+         ret = snprintf(buf+offset, len, " NEW");
+         BUFFER_SIZE(ret, size, len, offset);
+         break;
+      case NFCT_T_DESTROY:
+         ret = snprintf(buf+offset, len, " DESTROY");
+         BUFFER_SIZE(ret, size, len, offset);
+         break;
+      default:
+         break;
+   }
+
+   ret = snprintf(buf+offset, len, " id=%u", nfct_get_attr_u32(ct,ATTR_ID));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " proto=%s", proto_str);
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-src=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-dst=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-sport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " orig-dport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " repl-src=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " repl-dst=%s", 
+         net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " repl-sport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_SRC)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   ret = snprintf(buf+offset, len, " repl-dport=%d", 
+         ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_DST)));
+   BUFFER_SIZE(ret, size, len, offset);
+
+   buf[size+1 > len ? len-1 : size] = '\0';
+
+   printf("%s\n",buf);
+   fflush(stdout);
+}
+
 static int event_cb(enum nf_conntrack_msg_type type,
 		    struct nf_conntrack *ct,
 		    void *data) {
    struct conntrack_list *no;
-   char str_time[LENGTH_ASCTIME+1];
    u_int8_t l4proto;
-   time_t now = time(NULL);
 
    // we are interested only in SNAT connections
    if (!nfct_getobjopt(ct, NFCT_GOPT_IS_SNAT))
@@ -66,25 +223,8 @@ static int event_cb(enum nf_conntrack_msg_type type,
    if (l4proto != IPPROTO_TCP && l4proto != IPPROTO_UDP)
       return NFCT_CB_CONTINUE;
 
-   strftime(str_time,LENGTH_ASCTIME,"%Y-%m-%d %H:%M:%S %z",localtime(&now));
-
-   if (debug) {
-      printf("%s %s [DEBUG] :: "
-            "%s %s id=%u "
-            "orig-src=%s orig-dst=%s orig-sport=%d orig-dport=%d "
-            "repl-src=%s repl-dst=%s repl-sport=%d repl-dport=%d\n",
-            str_time, PROGNAME,
-            (type==NFCT_T_NEW) ? "NEW" : "DESTROY",
-            proto_str(l4proto),
-            nfct_get_attr_u32(ct,ATTR_ID),
-            net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC)),
-            net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_DST)),
-            ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC)),
-            ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_DST)),
-            net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_SRC)),
-            net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_DST)),
-            ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_SRC)),
-            ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_DST)));
+   if (debug_flag) {
+      print_debug(ct, type, proto_str(l4proto));
    }
 
    switch(type) {
@@ -93,7 +233,7 @@ static int event_cb(enum nf_conntrack_msg_type type,
          no->id = nfct_get_attr_u32(ct,ATTR_ID);
          no->orig_ipv4_src = nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC);
          no->orig_port_src = nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC);
-         no->timestamp = now;
+         time(&no->timestamp);
          list_add(&ct_list, no);
          break;
       case NFCT_T_DESTROY:
@@ -102,15 +242,7 @@ static int event_cb(enum nf_conntrack_msg_type type,
                nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC),
                nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC));
          if (no) {
-            printf("End of a %s SNAT connection: Original_Src: %s/%u "
-                  "Translated_Src: %s/%u "
-                  "Lifetime: %ld seconds\n",
-                  proto_str(l4proto),
-                  net2addr(nfct_get_attr_u32(ct,ATTR_ORIG_IPV4_SRC)),
-                  ntohs(nfct_get_attr_u16(ct,ATTR_ORIG_PORT_SRC)),
-                  net2addr(nfct_get_attr_u32(ct,ATTR_REPL_IPV4_DST)),
-                  ntohs(nfct_get_attr_u16(ct,ATTR_REPL_PORT_DST)),
-                  time(NULL) - no->timestamp);
+            print_snatlog(ct, &no->timestamp, proto_str(l4proto));
             list_del(&ct_list,no);
          }
          break;
@@ -118,22 +250,72 @@ static int event_cb(enum nf_conntrack_msg_type type,
          break;
    }
 
-   fflush(stdout);
-
    return NFCT_CB_CONTINUE;
 }
 
 static void event_sighandler(int s) {
    nfct_close(cth);
-   fprintf(stderr, "%s :: finishing...\n",PROGNAME);
+   printf("%s :: finishing...\n",PROGNAME);
    exit(EXIT_SUCCESS);
 }
 
-void print_error(const char *msg, int errnum) {
+void print_error(const char *msg) {
    fprintf(stderr,"[ERROR] %s :: %s (%s)\n",PROGNAME, msg, strerror(errnum));
 }
 
+void print_out(const char *msg) {
+   if (daemon_flag) {
+      syslog(LOG_INFO, msg);
+   } else {
+      printf("%s\n",msg);
+      fflush(stdout);
+   }
+}
+
 int main(int argc, char *argv[]) {
+   int c;
+
+   while (1) {
+      static struct option long_options[] = {
+         /* Flags */
+         {"debug",   no_argument,       0, 'd'},
+         {"daemon",  no_argument,       0, 's'},
+         {"help",    no_argument,       0, 'h'},
+         {"facility",required_argument, 0, 'f'},
+         {0, 0, 0, 0}
+      };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+      
+      c = getopt_long (argc, argv, "dshf:",long_options, &option_index);
+
+      if (c == -1)
+         break;
+
+      switch(c) {
+         case 'd':
+            debug_flag = 1;
+            break;
+         case 's':
+            daemon_flag = 1;
+            break;
+         case 'h':
+            usage();
+            exit(EXIT_SUCCESS);
+            break;
+         case 'f':
+            syslog_facility = __str2facility(optarg);
+            if (syslog_facility == -1) {
+               errno = EINVAL;   
+               fprintf(stderr, "Invalid syslog facility: %s\n", 
+                     optarg, PROGNAME);
+               usage();
+               exit(EXIT_FAILURE);
+            }
+
+      }
+
+   }
 
    cth = nfct_open(CONNTRACK,
          NF_NETLINK_CONNTRACK_NEW|NF_NETLINK_CONNTRACK_DESTROY);
